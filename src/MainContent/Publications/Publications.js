0d50a7f7cd75e72bd590744d1aa4772bdbaaec98
damik3 <michalis.vargiamis@gmail.com>
2024-02-13 17:29:23 +0200

    Publications are now read from bibtext.bib

--- src/MainContent/Publications/Publications.js
+++ src/MainContent/Publications/Publications.js
@@ -7,97 +7,24 @@ function Publications() {
     const [citations, setCitations] = useState([]);
 
     useEffect(() => {
-        // Define your BibTeX string here
-        const bibTeXString = `
-@article{chambers2023variable,
-  author={Vaughn Chambers, Bradley Hobbs, William Gaither, Zachary The, Anthony Zhou, Chrysostomos Karakasis, and Panagiotis Artemiadis},
-  booktitle={ASME Journal of Mechanisms and Robotics},
-  title={The Variable Stiffness Treadmill (VST) 2.0: Development and Validation of a Unique Tool to
-Investigate Locomotion on Compliant Terrains},
-  volume={},
-  year={2023},
-  note={Under Review}
-}
+        fetch("bibtex.bib")
+            .then((response) => response.text())
+            .then((bibTeXString) => {
+                console.log("Raw BibTeX Data:", bibTeXString);
 
-@inproceedings{karakasis2023f,
-  author={Chrysostomos Karakasis, Robert Salati, and Panagiotis Artemiadis},
-  booktitle={2023 IEEE/RSJ International Conference on Intelligent Robots and Systems (IROS)},
-  title={Adjusting the Quasi-Stiffness of an Ankle-Foot Prosthesis Improves Walking Stability during Locomotion over Compliant Terrain},
-  year={2023},
-  volume={},
-  number={},
-  pages={2140-2145},
-  keywords={Legged locomotion;Instruments;Green products;Robustness;Safety;Admittance;Standards},
-  doi={10.1109/IROS55552.2023.10342344}}
+                const parsedData = BibtexParser.toJSON(bibTeXString);
+                console.log("Parsed Data:", parsedData);
 
-@article{karakasis2023energy,
-  author = {Chrysostomos Karakasis, Ioannis Poulakakis, and Panagiotis Artemiadis},
-  title = "An Energy-Based Framework for Robust Dynamic Bipedal Walking Over Compliant Terrain",
-  booktitle = {Journal of Dynamic Systems, Measurement, and Control},
-  pages = {1-12},
-  year = {2023},
-  month = {11},
-  abstract = "{Bipedal locomotion over compliant terrain is an important and largely underexplored problem in the robotics community. Although robot walking has been achieved on some non-rigid surfaces with existing control methodologies, there is a need for a systematic framework applicable to different bipeds that enables stable locomotion over various compliant terrains. In this work, a novel energy-based framework is proposed that allows the dynamic locomotion of bipeds across a wide range of compliant surfaces. The proposed framework utilizes an extended version of the 3D Dual Spring-Loaded Inverted Pendulum (Dual-SLIP) model that supports compliant terrains, while a bioinspired controller is employed to regulate expected perturbations of extremely low ground-stiffness levels. An energy-based methodology is introduced for tuning the bioinspired controller to enable dynamic walking with robustness to a wide range of low ground-stiffness one-step perturbations. The proposed system and controller are shown to mimic the vertical Ground Reaction Force (GRF) responses observed in human walking over compliant terrains. Moreover, they succeed in handling repeated unilateral stiffness perturbations under specific conditions. This work can advance the field of biped locomotion by providing a biomimetic method for generating stable human-like walking trajectories for bipedal robots over various compliant surfaces. Furthermore, the concepts of the proposed framework could be incorporated into the design of controllers for lower-limb prostheses with adjustable stiffness to improve their robustness over compliant surfaces.}",
-  issn = {0022-0434},
-  doi = {10.1115/1.4064094},
-  url = {https://doi.org/10.1115/1.4064094},
-  eprint = {https://asmedigitalcollection.asme.org/dynamicsystems/article-pdf/doi/10.1115/1.4064094/7060193/ds-23-1181.pdf},
-}
+                // Sort citations by year (latest to oldest, if needed)
+                // parsedData.sort((a, b) => b.fields.year - a.fields.year);
+                console.log("Sorted Data:", parsedData);
 
-@inproceedings{karakasis2022,
-  author={Chrysostomos Karakasis, Ioannis Poulakakis, and Panagiotis Artemiadis},
-  booktitle={2022 30th Mediterranean Conference on Control and Automation (MED)},
-  title={Robust Dynamic Walking for a 3D Dual-SLIP Model under One-Step Unilateral Stiffness Perturbations: Towards Bipedal Locomotion over Compliant Terrain},
-  year={2022},
-  volume={},
-  number={},
-  pages={969-975},
-  doi={10.1109/MED54222.2022.9837236}
-}
-
-@article{karakasis2021real,
-  title={Real-time kinematic-based detection of foot-strike during walking},
-  author={Chrysostomos Karakasis and Panagiotis Artemiadis},
-  booktitle={Journal of Biomechanics},
-  pages={110849},
-  year={2021},
-  publisher={Elsevier}
-}
-
-@inproceedings{karakasis2021f,
-  title={F-VESPA: A Kinematic-based Algorithm for Real-time Heel-strike Detection During Walking},
-  author={Chrysostomos Karakasis and Panagiotis Artemiadis},
-  booktitle={2021 IEEE/RSJ International Conference on Intelligent Robots and Systems (IROS)},
-  pages={5098--5103},
-  year={2021},
-  organization={IEEE}
-}
-
-
-@inproceedings{karakasis2020exploiting,
-  title={Exploiting the SoC FPGA Capabilities in the Control Architecture of a Quadruped Robot},
-  author={Chrysostomos Karakasis, Konstantinos Machairas, Charalampos Marantos, Iosif S. Paraskevas, Evangelos Papadopoulos, and Dimitrios Soudris},
-  booktitle={2020 IEEE/ASME International Conference on Advanced Intelligent Mechatronics (AIM)},
-  pages={501--507},
-  year={2020},
-  organization={IEEE},
-  url={https://ieeexplore.ieee.org/document/9159012}
-}
-      // Add more BibTeX entries as needed
-    `;
-
-        console.log("Raw BibTeX Data:", bibTeXString);
-
-        const parsedData = BibtexParser.toJSON(bibTeXString);
-        console.log("Parsed Data:", parsedData);
-
-        // Sort citations by year (latest to oldest)
-        // parsedData.sort((a, b) => b.fields.year - a.fields.year);
-        console.log("Sorted Data:", parsedData);
-
-        setCitations(parsedData);
-        // Add this line to log the updated citations array
-        console.log("Updated Citations:", citations);
+                setCitations(parsedData);
+            })
+            .catch((error) => {
+                console.error("Error fetching or parsing BibTeX data:", error);
+                // Handle errors appropriately, e.g., display an error message
+            });
     }, []);
 
     return (
