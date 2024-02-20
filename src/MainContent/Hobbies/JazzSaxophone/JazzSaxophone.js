4820aa75d5b3ac768687ae926154c4e427e02a7b
Chrysostomos Karakasis <chryskar@udel.edu>
2024-02-20 04:17:58 -0500

    Finished off first complete draft, by adding stuff under all projects and adding another hobby.

--- src/MainContent/Hobbies/JazzSaxophone/JazzSaxophone.js
+++ src/MainContent/Hobbies/JazzSaxophone/JazzSaxophone.js
@@ -6,7 +6,7 @@ function JazzSaxophone() {
                 <h2 className="fw-bold m-3">Jazz Saxophonist at the NTUA Conservatory</h2>
 
                 <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-                    <iframe className="m-3" width="560" height="315" src="https://www.youtube.com/embed/5OgnfzQ01m4?si=GT9JYG4CR1_YUuHA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                    <iframe className="m-3" width="560" height="315" src="https://www.youtube.com/embed/5OgnfzQ01m4?si=GT9JYG4CR1_YUuHA" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                 </div>
                 <span className="text-body m-3">
                     As a sophomore I started taking music lessons, despite my age and complete lack of experience. Today, I have learned how to play the saxophone and occasionally perform jazz pieces with friends privately or in jam sessions.
