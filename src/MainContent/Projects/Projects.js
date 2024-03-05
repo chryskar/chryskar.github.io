2869bb601b83f05767b284aa46720d985a580ee8
Chrysostomos Karakasis <chryskar@udel.edu>
2024-03-05 14:49:17 -0500

    Updated projects.

--- src/MainContent/Projects/Projects.js
+++ src/MainContent/Projects/Projects.js
@@ -2,10 +2,11 @@ import laelaps from "./img/laelapsII.jpg";
 import vst from "./img/vicon_mocap.jpg";
 import foot_strike from "./img/foot-strike.png";
 import line_tracking_robot from "./img/front_view_robot.jpg";
-import dual_slip from "./DualSLIP/img/dual_slip.jpg"
-import ankle_prosthesis from "./AnkleProsthesis/img/IROS23_1579_graphical_abstract.jpg"
+import dual_slip from "./img/dual_slip.jpg"
+import ankle_prosthesis from "./AnkleProsthesis/img/subject_front_view.jpg"
 import personal_webpage from "./img/personal_webpage.png"
 import {Link} from "react-router-dom";
+import './BoxWithImage_projects.css'; // Import your CSS file
 
 function Projects () {
     return (
@@ -15,46 +16,97 @@ function Projects () {
                 <div className="container d-flex flex-wrap justify-content-center">
 
                     <Link to="ankle-prosthesis" className="d-flex flex-column align-items-center m-5 clickable" style={{ color: 'black' }}>
-                        <img src={ankle_prosthesis} alt="Logo 1" className="section-logo m-2" style={{ width: 'auto', maxHeight: '250px'}}/>
-                        <h4 className="fw-bold m-2">Ankle-foot Prosthesis</h4>
-                        {/*<span className="m-2">2020-2023</span>*/}
+                        {/*<img src={ankle_prosthesis} alt="Logo 1" className="section-logo m-2" style={{ width: 'auto', maxHeight: '250px'}}/>*/}
+                        <div className="card-shadow">
+                            <div className="card-projects">
+                                <div className="card-image-container-projects">
+                                    <img className="card-image-projects" src={ankle_prosthesis} id='prosthesis' alt="HORC"/>
+                                </div>
+                                <div className="card-title-projects fw-bold">Ankle-foot Prosthesis</div>
+                                <div className="card-subtext-projects fw-bold">2021-Present</div>
+                            </div>
+                        </div>
                     </Link>
 
                     <Link to="dual-slip" className="d-flex flex-column align-items-center m-5 clickable" style={{ color: 'black' }}>
-                        <img src={dual_slip} alt="Logo 1" className="section-logo m-2" style={{ width: 'auto', maxHeight: '250px'}}/>
-                        <h4 className="fw-bold m-2" style={{ textAlign: 'center' }}>3D Dual-Spring Loaded<br />Inverted Pendulum (SLIP)</h4>
-                        {/*<span className="m-2">2020-2023</span>*/}
+                        <div className="card-shadow">
+                            <div className="card-projects">
+                                <div className="card-image-container-projects">
+                                    <img className="card-image-projects" src={dual_slip} id='dual_slip' alt="HORC"/>
+                                </div>
+                                <div className="card-title-projects fw-bold">3D Dual-Spring Loaded Inverted Pendulum (SLIP)</div>
+                                <div className="card-subtext-projects fw-bold">2021-Present</div>
+                            </div>
+                        </div>
+
                     </Link>
 
                     <Link to="f-vespa" className="d-flex flex-column align-items-center m-5 clickable" style={{ color: 'black' }}>
-                        <img src={foot_strike} alt="Logo 1" className="section-logo m-2" style={{ width: 'auto', maxHeight: '250px'}}/>
-                        <h4 className="fw-bold m-2" style={{ textAlign: 'center' }}>Foot-strike Detection<br /> Algorithm (F-VESPA)</h4>
-                        {/*<span className="m-2">2021</span>*/}
+                        <div className="card-shadow">
+                            <div className="card-projects">
+                                <div className="card-image-container">
+                                    <img className="card-image-projects" src={foot_strike} alt="HORC"/>
+                                </div>
+                                <div className="card-title-projects fw-bold">Foot-strike Detection Algorithm (F-VESPA)</div>
+                                <div className="card-subtext-projects fw-bold">2020-2021</div>
+                            </div>
+                        </div>
+
                     </Link>
 
-                    <Link to="vst-architecture" className="d-flex flex-column align-items-center m-5 clickable" style={{ color: 'black' }}>
-                        <img src={vst} alt="Logo 1" className="section-logo m-2" style={{ width: 'auto', maxHeight: '250px'}}/>
-                        <h4 className="fw-bold m-2">Variable Stiffness Treadmill (VST)</h4>
-                        {/*<span className="m-2">2019-2023</span>*/}
+                    <Link to="vst-architecture" className="d-flex flex-column align-items-center m-5 clickable">
+                        <div className="card-shadow">
+                            <div className="card-projects">
+                                <div className="card-image-container">
+                                    <img className="card-image-projects" src={vst} id='VST' alt="HORC"/>
+                                </div>
+                                <div className="card-title-projects fw-bold">Variable Stiffness Treadmill (VST)</div>
+                                <div className="card-subtext-projects fw-bold">2019-2020</div>
+                            </div>
+                        </div>
                     </Link>
 
                     <Link to="master-thesis" className="d-flex flex-column align-items-center m-5 clickable" style={{ color: 'black' }}>
-                        <img src={laelaps} alt="Logo 2" className="section-logo m-2" style={{ width: 'auto', maxHeight: '250px'}}/>
-                        {/*,  objectFit: 'cover'*/}
-                        <h4 className="fw-bold m-2" style={{ textAlign: 'center' }}>SoC FPGA Architecture<br /> for Quadruped Robot</h4>
-                        {/*<span className="m-2">2018-2019</span>*/}
+                        {/*<img src={laelaps} alt="Logo 2" className="section-logo m-2" style={{ width: 'auto', maxHeight: '250px'}}/>*/}
+                        {/*<h4 className="fw-bold m-2" style={{ textAlign: 'center' }}>SoC FPGA Architecture<br /> for Quadruped Robot</h4>*/}
+
+                        <div className="card-shadow">
+                            <div className="card-projects">
+                                <div className="card-image-container">
+                                    <img className="card-image-projects" src={laelaps} id='' alt="HORC"/>
+                                </div>
+                                <div className="card-title-projects fw-bold">SoC FPGA Architecture for Quadruped Robot</div>
+                                <div className="card-subtext-projects fw-bold">2018-2019</div>
+                            </div>
+                        </div>
+
                     </Link>
 
                     <Link to="line-tracking-robot" className="d-flex flex-column align-items-center m-5 clickable" style={{ color: 'black' }}>
-                        <img src={line_tracking_robot} alt="Logo 1" className="section-logo m-2" style={{ width: 'auto', maxHeight: '250px'}}/>
-                        <h4 className="fw-bold m-2" style={{ textAlign: 'center' }}>Line Tracking Robot</h4>
-                        {/*<span className="m-2">2020-2023</span>*/}
+                        <div className="card-shadow">
+                            <div className="card-projects">
+                                <div className="card-image-container">
+                                    <img className="card-image-projects" src={line_tracking_robot} id='projects' alt="HORC"/>
+                                </div>
+                                <div className="card-title-projects fw-bold">Line Tracking Robot</div>
+                                <div className="card-subtext-projects fw-bold">2021</div>
+                            </div>
+                        </div>
+
+
                     </Link>
 
                     <Link to="personal-webpage" className="d-flex flex-column align-items-center m-5 clickable" style={{ color: 'black' }}>
-                        <img src={personal_webpage} alt="Logo 2" className="section-logo m-2" style={{ width: 'auto', maxHeight: '250px', maxWidth: '100%'}}/>
-                        <h4 className="fw-bold m-2" style={{ textAlign: 'center' }}>Personal Webpage</h4>
-                        {/*<span className="m-2">2018-2019</span>*/}
+                        <div className="card-shadow">
+                            <div className="card-projects">
+                                <div className="card-image-container">
+                                    <img className="card-image-projects" src={personal_webpage} id='personal-webpage' alt="HORC"/>
+                                </div>
+                                <div className="card-title-projects fw-bold">Personal Webpage</div>
+                                <div className="card-subtext-projects fw-bold">2024-Present</div>
+                            </div>
+                        </div>
+
                     </Link>
 
                 </div>
